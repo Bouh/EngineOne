e78c92fcac94424d993f751eb72d0ba66f79fce8
Bouh <bouhvivez@gmail.com>
2014-05-21 12:14:19 +0200

    First Commint
    Base de collison avec cannon.js et three.js

---
+++ js/updateStage.js
@@ -0,0 +1,15 @@
+//ok
+app.log_console === true ? console.log("Open -- updateStage.js") : false ;
+
+app.updateStage = function(){
+
+	app.controller();//Gestion interface/code
+	
+	
+
+	
+	
+	
+	
+};
+
